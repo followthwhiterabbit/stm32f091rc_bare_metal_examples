310cf8fed82ca342eb46bd5c0ff1e33165e1a23b
Seyit Kaan Karaalioglu <kaankaraalioglu@yahoo.com>
2023-11-04 12:57:23 +0100

    adxl345 accelerometer is being added

---
+++ 21_I2C_ADXL345/Inc/i2c.h
@@ -0,0 +1,15 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Nov 3, 2023
+ *      Author: kaan
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+
+
+#endif /* I2C_H_ */
---
+++ 21_I2C_ADXL345/Src/main.c
@@ -0,0 +1,69 @@
+//#include "stm32f091xc.h"
+#include "stm32f0xx.h"
+#include "uart.h"
+#include <stdint.h>
+#include <stdio.h>
+
+
+#define GPIOA_ENABLE (1U << 17)
+
+
+#define GPIOA_5 (1U<<5);
+
+
+char key;
+
+
+int main(void)
+{
+	RCC->AHBENR |= GPIOA_ENABLE;
+
+	GPIOA->MODER |= (1U << 10);
+	GPIOA->MODER &= ~(1U << 11);
+
+
+	uart2_rx_interrupt_init();
+	//uart2_tx_init();
+	//uart2_rxtx_init();
+
+
+while(1)
+{
+
+}
+
+
+
+}
+
+
+
+
+static void uart_callback(void)
+{
+	key = USART2->RDR;
+
+	if(key == '1')
+	{
+		GPIOA->ODR |= GPIOA_5;
+		printf("1 is pressed\n");
+	}
+	else
+	{
+		GPIOA->ODR &= ~GPIOA_5;
+		printf("another key is pressed\n");
+	}
+
+}
+
+
+void USART2_IRQHandler(void)
+{
+	/* Check if RXNE is set */
+if(USART2->ISR & ISR_RXNE)
+{
+	uart_callback();
+}
+
+}
+
